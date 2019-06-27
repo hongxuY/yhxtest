9c36b7c4f555e02a8514eb5f4c61f6a118a8b098
hongxuY <1944783476@qq.com>
2019-06-27 14:47:40 +0800

    update

---
+++ test3.py
@@ -0,0 +1,13 @@
+#coding:utf-8
+a2=raw_input("请输入a2")
+a1=input("请输入a1")
+
+
+# print (type(a1))
+# print (type(a2))
+# a1=int(a1)
+# a2=int(a2)
+print (type(a1))
+print (type(a2))
+print (a1)
+print (a2)
